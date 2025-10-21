b462ab876078f781051df9b5509319cbd34d3880
Mohammed Raziq V <anuvraziq@gmail.com>
2025-10-21 09:57:49 +0530

    minor improvements 2

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,15 @@ import taskRouter from './routes/taskRoute.js';
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Database Connection
+
 connectDB();
 
-// Routes
+
 app.use("/api/user", userRouter);
 
 app.use('/api/tasks', taskRouter);
